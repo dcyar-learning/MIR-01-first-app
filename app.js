553f14c9c3b9429a76f1a5e03c2faea102ba9a77
Jhon Achata Limachi <jhonal183@gmail.com>
2022-01-27 19:17:00 -0500

    updates

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 
 app.get('/makers/:nombre', (req, res) => {
-  const nombre = req.params.nombre;
+  const [first, ...rest] = req.params.nombre;
 
-  res.send(`<h1>Hola ${nombre}!</h1>`);
+  res.send(`<h1>Hola ${first.toUpperCase() + rest.join('')}!</h1>`);
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
