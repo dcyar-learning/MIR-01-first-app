7d2433eecd4956c23c439ccf3b50c6ec9eafa1bf
Jhon Achata Limachi <jhonal183@gmail.com>
2022-02-02 10:18:08 -0500

    Saludame 3

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-  let message = '';
+app.set('view engine', 'pug')
+app.set('views', 'views')
 
-  for (let i = 1; i <= 50; i++) {
-    message += '<p>';
-    message += (i % 2 === 0) ? `${i} Soy Par!` : `${i} Soy Impar!`;
-    message += '</p>';
-  }
+app.use(express.urlencoded({ extended: true }))
 
-  res.send(message);
-});
+app.get('/', (req, res) => res.render('form'))
+app.post('/', (req, res) => {
+  const {name} = req.body
+
+  res.render('home', {name})
+})
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
