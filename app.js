f9f77823af4a78d8eba8c551f56d4e71e0b0622b
Jhon Achata Limachi <jhonal183@gmail.com>
2022-02-07 21:23:42 -0500

    Formulario de registro

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const { restart } = require('nodemon');
 const app = express()
 require('dotenv').config()
 
@@ -7,12 +8,13 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1',
 
 mongoose.connection.on('error', (err) => console.log(err))
 
-const visitorSchema = mongoose.Schema({
+const userSchema = mongoose.Schema({
   name: mongoose.Schema.Types.String,
-  count: mongoose.Schema.Types.Number
+  email: mongoose.Schema.Types.String,
+  password: mongoose.Schema.Types.String,
 })
 
-const Visitor = mongoose.model('Visitor', visitorSchema)
+const User = mongoose.model('User', userSchema)
 
 app.set('view engine', 'pug')
 app.set('views', 'views')
@@ -20,30 +22,39 @@ app.set('views', 'views')
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', async (req, res) => {
-  const {name = 'Anónimo'} = req.query
-
   try {
-    const visitor = await Visitor.findOne({name})
+    const users = await User.find()
 
-    if(!visitor) new Error('No existe el visitante')
-
-    if (visitor && name !== 'Anónimo') {
-      visitor.count++
-      await visitor.save()
-    } else {
-      await Visitor.create({name, count: 1})
-    }
-
-    const visitors = await Visitor.find()
-
-    if(!visitors) new Error('No hay visitantes')
+    if(!users) new Error('No existen los usuarios')
 
     res.render('home', {
-      visitors
+      users
     })
   } catch (err) {
     res.status(500).send(err.message)
   }
 });
+
+app.post('/', async (req, res) => {
+  const {name, email, password} = req.body
+
+  try {
+    const user = new User({
+      name,
+      email,
+      password
+    })
+
+    await user.save()
+
+    res.redirect('/')
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
+})
+
+app.get('/register', (req, res) => {
+  res.render('form')
+})
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
