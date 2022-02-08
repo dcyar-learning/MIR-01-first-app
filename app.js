0f4887ceb57062477e0dc6a7129b6b7f879ff523
Jhon Achata Limachi <jhonal183@gmail.com>
2022-02-08 10:33:51 -0500

    Devolviendo JSON

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const cookieSession = require('cookie-session')
-const bcrypt = require('bcrypt')
 const app = express()
 require('dotenv').config()
 
@@ -9,13 +7,13 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1',
 
 mongoose.connection.on('error', (err) => console.log(err))
 
-const userSchema = mongoose.Schema({
+const productSchema = mongoose.Schema({
   name: mongoose.Schema.Types.String,
   email: mongoose.Schema.Types.String,
   password: mongoose.Schema.Types.String,
 })
 
-userSchema.statics.authenticate = async (email, password) => {
+productSchema.statics.authenticate = async (email, password) => {
   const user = await mongoose.model('User').findOne({ email });
 
   if(!user) return null
@@ -29,90 +27,20 @@ userSchema.statics.authenticate = async (email, password) => {
   });
 };
 
-const User = mongoose.model('User', userSchema)
+const Product = mongoose.model('Product', productSchema)
 
-app.set('view engine', 'pug')
-app.set('views', 'views')
-
-app.use(express.urlencoded({ extended: true }))
-app.use(cookieSession({
-  secret: process.env.COOKIE_SECRET || 'secret',
-  maxAge: 24 * 60 * 60 * 1000
-}))
-
-const requireUser = async (req, res, next) => {
-  const userId = req.session.userId;
-
-  if (userId) {
-    const user = await User.findOne({ _id: userId });
-    res.locals.user = user;
-    next();
-  } else {
-    return res.redirect("/login");
-  }
-}
-
-app.get('/', requireUser, async (req, res) => {
+app.get('/products', async (req, res) => {
   try {
-    const users = await User.find()
+    const products = await Product.find()
 
-    if(!users) new Error('No existen los usuarios')
+    if(!products) new Error('No existen los productos')
 
-    res.render('home', {
-      users
-    })
+    res.status(200).json(products)
   } catch (err) {
-    res.status(500).send(err.message)
+    res.status(500).json({
+      message: err.message || 'Algo salio mal'
+    })
   }
 });
-
-app.get('/login', (req, res) => {
-  res.render('login-form')
-})
-
-app.post('/login', async (req, res, next) => {
-  const { email, password } = req.body
-
-  try {
-    const user = await User.authenticate(email, password)
-    
-    if(!user) throw new Error('Wrong email or password. Try again!')
-    
-    req.session.userId = user._id
-
-    res.redirect('/')
-  } catch (err) {
-    res.render('login-form', {
-      error: err.message
-    })
-  }
-})
-
-app.get('/register', (req, res) => {
-  res.render('register-form')
-})
-
-app.post('/register', async (req, res) => {
-  const {name, email, password} = req.body
-
-  try {
-    const user = new User({
-      name,
-      email,
-      password: await bcrypt.hash(password, 10)
-    })
-
-    await user.save()
-
-    res.redirect('/')
-  } catch (err) {
-    res.status(500).send(err.message)
-  }
-})
-
-app.get('/logout', (req, res) => {
-  req.session = null
-  res.redirect('/login')
-})
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
